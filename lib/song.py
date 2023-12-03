59f285ff061f0d8e7eb434aa45d9bd14f822ffc7
joshua chivile <joshuachivile@gmail.com>
2023-12-03 16:42:28 +0300

    Class Attrinutes and Methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,48 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    start_count = 1
+    artist_count = {}
+    artist_start_count = 1
+
+    
+    def __init__(self,name,artist,genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_song_to_count()
+    
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls,genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+            cls.genre_count[genre] = cls.start_count
+        else:
+            cls.start_count += 1
+            cls.genre_count[genre] = cls.start_count 
+
+    @classmethod
+    def add_to_artists(cls,artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+            cls.artist_count[artist] = cls.artist_start_count
+        else:
+            cls.artist_start_count +=1
+            cls.artist_count[artist] = cls.artist_start_count
+    
+    @classmethod
+    def add_to_genre_count(cls):
+        return cls.genre_count
+    
+    @classmethod
+    def add_to_artist_count(cls):
+        return cls.artist_count
+        
